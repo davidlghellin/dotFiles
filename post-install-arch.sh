47691e111987e5b7c89b7b6512d2512fb1285102
David López <davidlghellin@gmail.com>
2023-08-04 21:02:52 +0200

    refactor

--- post-install-arch.sh
+++ post-install-arch.sh
@@ -1,55 +1,80 @@
 #!/bin/bash
 
-sudo pacman -S nano git terminator vlc vi
-# color, paralell, ILoveCandy
+## Aumentar el numero de cpus disponibles para compilar los package
+sudo sed 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j${nproc}"/' /etc/makepkg.conf
+sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
+sudo sed -i 's/#ParallelDownloads/ParallelDownloads/g' /etc/pacman.conf
+# ILoveCandy
 
-sudo pacman -S network-manager-applet alsa-utils telegram-desktop 
+sudo pacman -S --noconfirm \
+    alsa-utils \
+    nano \
+    git \
+    vlc \
+    vi \
+    neovim \
+    terminator \
+    kitty \
+    htop \
+    ranger \
+    cmus \
+    neofetch \
+    tig \
+    bat \
+    exa \
+    ripgrep \
+    notify-osd \
+    telegram-desktop \
+    firefox-i18n-es-es \
+    transmission-gtk \
+    yt-dlp \
+    meld \
+    foliate \
+    calibre
+    # visor epub
+sudo pacman -S --noconfirm network-manager-applet
 #sudo systemctl enable NetworkManager.service
 
-sudo pacman -S foliate calibre # visor epub 
-
-lscpu | grep '^CPU(s):'
-sudo nano /etc/makepkg.conf # cpu + 1
-
-sudo pacman -S firefox-i18n-es-es meld
-sudo pacman -S zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #chsh -s /bin/zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+sudo pacman -S lxappearance gnome-tweaks # apariencia
+sudo pacman -S xorg-xrandr arandr # para posición del monitor
+sudo pacman -S scrot # hacer pantallazos
+sudo pacman -S fd # find simplicifado 
+sudo pacman -S procs # info de procesos
 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-yay -S radiotray streamripper # streamripper http://1a-dance.radionetz.de/1a-dance.mp3 -d ~/Grabaciones
-yay -S brave-bin visual-studio-code-bin #para usar el devcontainer
-yay -S caffeine-ng
+yay -S --noconfirm radiotray \
+    caffeine-ng \
+    brave-bin \
+    visual-studio-code-bin #para usar el devcontainer
+yay -S --noconfirm streamripper # streamripper http://1a-dance.radionetz.de/1a-dance.mp3 -d ~/Grabaciones
 
 sudo pacman -S docker docker-compose
 sudo usermod -aG docker $USER	
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 
-sudo pacman -S transmission-gtk htop bat exa ripgrep notify-osd picom
-sudo pacman -S xorg-xrandr arandr # para posición del monitor
-sudo pacman -S ranger cmus tig yt-dlp neovim scrot 
-sudo pacman -S lxappearance gnome-tweaks 
-sudo pacman -S fd procs # programas rust, utiles
-
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
 # dar vida a batería
 yay -S auto-cpufreq
 sudo systemctl start auto-cpufreq
 sudo systemctl enable auto-cpufreq
-# sudo  auto-cpufreq --stats
+# sudo auto-cpufreq --stats
 
-sudo pacman -S awesome
+sudo pacman -S awesome picom
 mkdir -p ~/.config/awesome
 git clone https://github.com/streetturtle/awesome-wm-widgets.git ~/.config/awesome
 
-
-https://github.com/horst3180/arc-icon-theme#installation
+#https://github.com/horst3180/arc-icon-theme#installation
+#https://nvchad.com/docs/quickstart/install
 
 # touchpad -> copiar el sudo cp /usr/share/X11/xorg.conf.d/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
 # añadir la conf que dicen en la wiki de arch https://wiki.archlinux.org/title/Touchpad_Synaptics#Frequently_used_options
